b9ccff1cc76cc7a620606f422f12c7e9fdf49d1d
pupptmstr <payforplayonsteam@yandex.ru>
2020-11-12 15:19:58 +0300

    Started doing search

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -6,18 +6,22 @@ function Header() {
     return (
         <div className={"header"}>
 
-            <a className={'logo-home-link'} href={"/"}><img src={logo} className='header-logo' alt='logo' width={240} height={103}/></a>
+            <a className={'logo-home-link'} href={"/"}>
+                <img src={logo} className='header-logo' alt='logo' width={240} height={103}/>
+            </a>
 
             <ul>
-                <li><a className={'header-link'} href="/#news">Блог</a></li>
-                <li><a className={'header-link'} href="/#team">Наша команда</a></li>
-                <li><a className={'header-link'} href="/#projects">Проекты</a></li>
-                <li><a className={'header-link'} href="/#support" style={{color:'#808080', opacity:0.6}}>Помощь</a></li>
+                <li><a className={'header-link'} href={"/#news"}>Блог</a></li>
+                <li><a className={'header-link'} href={"/#team"}>Наша команда</a></li>
+                <li><a className={'header-link'} href={"/#projects"}>Проекты</a></li>
+                <li>
+                    <a className={'header-link'} href={"/#support"} style={{color: '#808080', opacity: 0.6}}>Помощь</a>
+                </li>
             </ul>
 
             <div className="searchBlock">
                 <img src={magnifier} alt='search' width={17} height={17} className="magnifier"/>
-                <input type='search' className='search' placeholder="Поиск..."/>
+                <input name='query' type='search' className='search' placeholder="Поиск..."/>
             </div>
 
         </div>
