b8ec2c3973e5ac0b1f5c1360775748551783198c
pupptmstr <payforplayonsteam@yandex.ru>
2020-11-14 23:01:13 +0300

    Cosmetic fixes and footer links changed

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import vk from "../../resources/img/vk_small.png";
-import insta from "../../resources/img/insta_small.png";
-import whatsapp from "../../resources/img/messenger_small.png";
+import twitter from "../../resources/img/twitter.svg";
+import telegram from "../../resources/img/telegram.svg";
 import Header from "./Header";
 
 
@@ -10,9 +10,15 @@ function Footer() {
         <div>
             <Header/>
             <div className={'footer-content'}>
-                <a href={'https://vk.com/pupptmstr'} target="_blank"> <img src={vk} alt={'VK'}/> </a>
-                <a href={'https://instagram.com/pupptmstr/'} target="_blank"> <img src={insta} alt={'Instagram'}/> </a>
-                <a href={'https://wa.me/79967875622'} target="_blank"> <img src={whatsapp} alt={'WhatsApp'}/> </a>
+                <a href={'https://vk.com/pupptmstr'} target="_blank">
+                    <img src={vk} alt={'VK'} className={'out-footer-href'}/>
+                </a>
+                <a href={'https://twitter.com/pupptmstr_'} target="_blank">
+                    <img src={twitter} alt={'Twitter'} className={'out-footer-href'}/>
+                </a>
+                <a href={'https://t.me/pupptmstr/'} target="_blank">
+                    <img src={telegram} alt={'Telegram'} className={'out-footer-href'}/>
+                </a>
                 <span className={'footer-text'}> © 2020 PuppetMake studio. Все права защищены</span>
             </div>
         </div>
